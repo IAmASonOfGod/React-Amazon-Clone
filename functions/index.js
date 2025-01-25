d7ac335a7819b0abd2d149316b4452f688dde10c
Lesego Ratau <rataulesego95@gmail.com>
2025-01-25 20:43:18 +0200

    Build

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,7 @@ const stripe = require("stripe")(
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://sparkling-pastelito-15c4cd.netlify.app", 
-  })
-);
+app.use(cors());
 
 app.post("/payment/create", async (req, res) => {
   const { total } = req.body;
